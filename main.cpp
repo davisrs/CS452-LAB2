90c7f66ca675db7929ed8502fce96a46b0a673e2
davisrs <davisrs@clarkson.edu>
2014-02-03 22:48:27 -0500

    Code typed from book

--- main.cpp
+++ main.cpp
@@ -30,12 +30,39 @@ init (void)
 		{ 0.90, -0.85},
 		{ 0.90,  0.90},
 		{-0.85,  0.90}
-	}
+	};
+	
+	glGenBuffers(NumBuffers, Buffers);
+	glBindBuffer(GL_ARRAY_BUFFER, Buffers[ArrayBuffer]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(verices), vertices, GL_STATIC_DRAW);
+	
+	ShaderInfo shaders[] = {
+		{ GL_VERTEX_SHADER, "triangles.vert" },
+		{ GL_FRAGMENT_SHADER, "triangles.frag" },
+		{ GL_NONE, NULL }
+	};
+	
+	GLuint program = LoadShaders(shaders);
+	glUseProgram(Program);
+	
+	glVertexPointer(vPosition, 2, GL_FLOAT, GL_FALSE, 0, BUFFFER_OFFSET(0));
+	glEnableVertexAttribArry(vPosition);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 //	display
 ///////////////////////////////////////////////////////////////////////////////
+
+void
+display(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+	
+	glBindVertexArray(VAOs[Triangles]);
+	glDrawArrays(GL_TRIANGLES, 0, NumVertices);
+	
+	glFlush();
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 //	main
