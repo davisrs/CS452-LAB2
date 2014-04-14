284f2c112da93cbd1a163f1f311782d9170d4cdd
davisrs <davisrs@clarkson.edu>
2014-04-14 18:57:58 -0400

    Only around 8 errors now...  MAKING PROGRESS!

--- ShaderLoader.cpp
+++ ShaderLoader.cpp
@@ -20,14 +20,14 @@ readMeShaderFile(const char* shaderFileName){//The magic happens here
 	//Input:shaderFileName = the .glsl file on your hardrive that has your shader code in it... goes here
 	//Output: buffy = the string that holds all that glsl code
 	//Local Vars
-	FILE* readFile //This is the stuff in file that gets made into our string
-	long int size //This is the size of (num chars) that file -and hence in our char array/string //being long means its at least 32 bits--so we can have big files w/o issues
-	char* buffy //This is our string of stuff in that file//Angel used buf, I'm way too tired right now.
+	FILE* readInFile; //This is the stuff in file that gets made into our string
+	long size; //This is the size of (num chars) that file -and hence in our char array/string //being long means its at least 32 bits--so we can have big files w/o issues
+	char* buffy; //This is our string of stuff in that file//Angel used buf, I'm way too tired right now.
 	//Start coding
 	readInFile = fopen(shaderFileName, "r");//open as read only, last thing we want to do is change anything //be sure to close this later
-	if (readInFile == NULL ) { //If the file be empty
+	if (readInFile== NULL ) { //If the file be empty
 		return NULL;//return empty
-		std::cerr << "WARNING: The file you gave me " << s.readFile << " was empty. WTF?";//and yell at the programmer
+		std::cerr << "WARNING: The file you gave me " << shaderFileName << " was empty. WTF?";//and yell at the programmer
 	}//Enough yelling
 
 	fseek(readInFile, 0L, SEEK_END);//No binary header=No offset, Find the end of that File!
@@ -35,7 +35,7 @@ readMeShaderFile(const char* shaderFileName){//The magic happens here
 	
 	fseek(readInFile,0L,SEEK_SET);//having read to the end, lets read it again more carefully-- this takes us back to the beginning of that file
 	buffy = new char[size + 1];//allocate memory for the array of chars (named buffy -- I'm gonna regret this) which is the same size as the (file + 1)
-	fread(buffy, 1, size, readInfile);//read an array of size (size) consisting of 1 byte chars from the readInFile and store it into a block of memory which is of at least size (size) of file (buffy!)
+	fread(buffy, 1, size, readInFile);//read an array of size (size) consisting of 1 byte chars from the readInFile and store it into a block of memory which is of at least size (size) of file (buffy!)
 	buffy[size] = ' ';//make the very end of the buffer into a space (so we know when it ends?)
 	fclose(readInFile);//WE ARE DONE with that physical file! CLOSE THE FILE before anything unexpected/ stupid happens
 
@@ -45,13 +45,13 @@ readMeShaderFile(const char* shaderFileName){//The magic happens here
 //Having the shader code in our buffer, we can use that to create GLSL program oobjects to invoke!
 
 //Function #2
-GLuint InitShader(const char* vShaderFileName, const char* fShaderFileName){
+GLint InitShader(const char* vShaderFileName, const char* fShaderFileName){
 	//Given the names of our vertex and fragment shader files containing precious GLSL code on our hard drive, generate a GLSL program object!
         //Input:vShaderFileName = the .glsl file on your hardrive that has your vertex shader code in it... goes here
 	//fShaderFileName = the .glsl file on your hardrive that has your fragment shader code in it... goes here
         //Output: GLuint program //the program object containing all of aour shaders!
         //Local Vars
-	GLuint program //the program object which contains our shaders!
+	GLuint program; //the program object which contains our shaders!
 	const int numShaders = 2;//number of shaders in our array of shaders, typically 2, a vertex + a fragment shader
 
 	//Structure of a Shader
@@ -72,10 +72,10 @@ GLuint InitShader(const char* vShaderFileName, const char* fShaderFileName){
 		Shader& s = shaders[i];//create a pointer to the ith shader in the array? -- named s for ease of typing
 		s.source = readMeShaderFile(s.filename); //Swizzling is nice
 		if ( s.source == NULL ) {//if the shader file is empty
-			std:cerr << "Failed to read " << s.filename << std::endl;
+			std::cerr << "Failed to read " << s.filename << std::endl;
 			exit( EXIT_FAILURE );//Yell at the programmer and kill the process in FAILURE
 		}//end if statement
-	        GLuint shaderID;//this is the identifier handle number for a OpenGL shader object--there is one for each of our shaders [in this case 2: vertex and fragment]
+	        GLint shaderID;//this is the identifier handle number for a OpenGL shader object--there is one for each of our shaders [in this case 2: vertex and fragment]
 
 		shaderID = glCreateShader(s.type);//Create an empty shader object of appropriate type specified in the Shader array of the for loop counter and return a nonzero ID#
 		glShaderSource(shaderID, 1, (const CLchar**) &s.source, NULL);//replace the source code in a the empty shader object handled by shaderID with the string of GLSL code in s.source
@@ -89,11 +89,11 @@ GLuint InitShader(const char* vShaderFileName, const char* fShaderFileName){
 		glGetShaderiv( shaderID, GL_COMPILE_STATUS, &compiled );//sets compiled to GL_TRUE if last compile of shaderID successful
 		if ( !compiled ) {//if compilation fails
 			std::cerr << s.filename << " failed to compile:"<<std::endl;//yell at the programmer
-			GLUint logSize;//variable for logsize
+			GLint logSize;//variable for logsize
 			glGetShaderiv( shaderID, GL_INFO_LOG_LENGTH, &logSize );//sets value of logsize
-			char* logMSg = new char[logSize];//creates a dynamic string big enough to hold the compilation log's message
-			glGetShaderInfoLog( shader, logSize, NULL, logMsg );//return the information log for the shader object referred to by shaderID, allow a buffer of LogSize char in length and store it in logMsg //the length of the string in the log isn't required so that parameter is NULL
-			std:cerr << logMsg << endl;//Yell at the programmer whatever error was in the shader's compilation log when it failed
+			char* logMsg = new char[logSize];//creates a dynamic string big enough to hold the compilation log's message
+			glGetShaderInfoLog( shaderID, logSize, NULL, logMsg );//return the information log for the shader object referred to by shaderID, allow a buffer of LogSize char in length and store it in logMsg //the length of the string in the log isn't required so that parameter is NULL
+			std::cerr << logMsg << std::endl;//Yell at the programmer whatever error was in the shader's compilation log when it failed
 			delete [] logMsg;//Clean up after yourself! Remove dynamic objects when finished with them!
 			exit( EXIT_FAILURE );//having yelled at the programmer, kill the process in FAILURE
 		}//end if loop
